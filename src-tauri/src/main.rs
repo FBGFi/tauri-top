9edf5944da7f609c992a479a22a2a5a6f1c702da
Juho Härme <juho.harme@knowit.fi>
2024-09-20 14:21:34 +0300

    basic rust implementation and experimenting around

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,38 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use sysinfo::System;
+
+#[derive(Debug, serde::Serialize)]
+struct ProcessInfo {
+  name: String,
+  memory: u64,
+  cpu_usage: f32,
+  pid: i32,
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn greet() -> Vec<ProcessInfo> {
+  let mut sys: System = System::new_all();
+
+  sys.refresh_all();
+
+  return sys
+    .processes()
+    .iter()
+    .map(|(&pid, process)| ProcessInfo {
+      name: process.name().to_str().unwrap_or("Unknown").to_string(),
+      memory: process.memory(),
+      cpu_usage: process.cpu_usage(),
+      pid: pid.as_u32() as i32,
+    })
+    .collect();
 }
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder
+    ::default()
+    .invoke_handler(tauri::generate_handler![greet])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
